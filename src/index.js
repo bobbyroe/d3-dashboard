cec972b705f572be6b78867075c257f56bf8bc1e
Bobby Roe <bobbyroe@gmail.com>
2019-03-02 20:58:05 -0800

    d3.json().then()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import * as d3 from "d3";
-
 d3.select("#root")
     .append("h1")
     .append("text")
     .text(`D3 version: ${d3.version}`);
+
+// Loading external data
+const weather_promise = d3.json("./351_weather.json");
+weather_promise.then( (data) => {
+    console.log(data);
+}, (reason) => {
+    console.log(reason);
+});
